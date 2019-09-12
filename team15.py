290c591afb09db475f0f72dec63a0d98923c5a42
Mads <45373029+aster-isk@users.noreply.github.com>
2019-09-11 20:53:11 -0700

    Update team15.py

--- team15.py
+++ team15.py
@@ -1,12 +1,5 @@
 import random
 
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
 
 team_name = 'Ghost' # Only 10 chars displayed.
 strategy_name = 'Point Ratio'
